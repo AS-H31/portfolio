7ea9fa590ca79b308aeff3603610b866fccb6161
Ahmad Shiyar Hesso <58402886+AS-H31@users.noreply.github.com>
2024-10-21 19:58:40 +0200

    Contact and Works

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { SectionWrapper } from "../hoc";
+import { EarthCanvas } from "./canvas";
+import { slideIn } from "../utils/motion";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
-export default Contact
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {};
+
+  const handleSubmit = (e) => {};
+
+  return (
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+      Contact
+    </div>
+  );
+};
+
+export default Contact;
